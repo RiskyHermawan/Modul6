42d75953c59c67310508038811a54952516cc7b2
RiskyHermawan <64541424+RiskyHermawan@users.noreply.github.com>
2020-05-14 11:22:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/modul6/Konfigurasi.java
@@ -0,0 +1,23 @@
+package com.example.modul6;
+
+public class Konfigurasi {
+
+        public static final String URL_ADD="http://192.168.0.105/modul6/Insert.php";
+        public static final String URL_GET_ALL="http://192.168.0.105/modul6/Read.php";
+        public static final String URL_GET_MHS="http://192.168.0.105/modul6/Select.php?id=";
+        public static final String URL_UPDATE_MHS="http://192.168.0.105/modul6/Update.php";
+        public static final String URL_DELETE_MHS="http://192.168.0.105/modul6/Delete.php?id=";
+
+        public static final String KEY_MHS_ID = "id";
+        public static final String KEY_MHS_NAMA = "nama";
+        public static final String KEY_MHS_JURUSAN = "jurusan";
+        public static final String KEY_MHS_EMAIL = "email";
+
+        public static final String TAG_JSON_ARRAY= "result";
+        public static final String TAG_ID= "id";
+        public static final String TAG_NAMA = "nama";
+        public static final String TAG_JURUSAN = "jurusan";
+        public static final String TAG_EMAIL = "email";
+
+        public static final String MHS_ID = "mhs_id";
+}
